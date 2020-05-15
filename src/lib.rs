3d5cd4b6872a4baf1d67c5db35317796ca3a4731
Taiki Endo <te316e89@gmail.com>
2020-05-15 17:23:41 +0900

    Allow clippy::manual_non_exhaustive lint
    non_exhaustive requires Rust 1.40.

--- src/lib.rs
+++ src/lib.rs
@@ -49,8 +49,8 @@
 // It cannot be included in the published code because these lints have false positives in the minimum required version.
 #![cfg_attr(test, warn(single_use_lifetimes))]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take requires Rust 1.40
-#![allow(clippy::mem_replace_with_default)]
+// mem::take and #[non_exhaustive] requires Rust 1.40
+#![allow(clippy::mem_replace_with_default, clippy::manual_non_exhaustive)]
 
 // older compilers require explicit `extern crate`.
 #[allow(unused_extern_crates)]
