8e4aa92905805fb318b88c766c5c5e2d5e980ead
Taiki Endo <te316e89@gmail.com>
2021-12-13 11:08:39 +0900

    Update const_generics_defaults test
    const_generics_defaults stabilized in Rust 1.59.

--- tests/run-pass/const_generics_defaults.rs
+++ tests/run-pass/const_generics_defaults.rs
@@ -1,23 +1,20 @@
-#![feature(const_generics_defaults)]
+// TODO: move to tests/test.rs once Rust 1.59 become stable.
 
 // https://github.com/rust-lang/rust/tree/0ced530534de1a77504b6229e6303d37a12282ee/src/test/ui/const-generics/defaults
 
 use easy_ext::ext;
 
-#[rustfmt::skip]
 #[ext(Ext)]
 impl<const N: usize = 3> () {}
 impl Ext for u8 {}
 
 // The code above is equivalent to the code below.
 
-#[rustfmt::skip]
 trait Trait<const N: usize = 3> {}
 impl<const N: usize> Trait<N> for () {}
 impl Trait for u8 {}
 
 // https://github.com/rust-lang/rust/blob/716394d6581b60c75cfdd88b8e5b876f2db88b62/src/test/ui/const-generics/defaults/const-param-as-default-value.rs
-#[rustfmt::skip]
 #[ext(Ext2)]
 impl<const N: usize, const M: usize = N> () {}
 
