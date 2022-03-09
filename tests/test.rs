08456e8ac859e6642be526635a1c8e043b4346eb
Taiki Endo <te316e89@gmail.com>
2022-03-09 22:34:23 +0900

    Move const_generics_defaults test to tests/test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -474,6 +474,25 @@ fn min_const_generics() {
 }
 
 #[test]
+fn const_generics_defaults() {
+    // https://github.com/rust-lang/rust/tree/0ced530534de1a77504b6229e6303d37a12282ee/src/test/ui/const-generics/defaults
+
+    #[ext(Ext)]
+    impl<const N: usize = 3> () {}
+    impl Ext for u8 {}
+
+    // The code above is equivalent to the code below.
+
+    trait Trait<const N: usize = 3> {}
+    impl<const N: usize> Trait<N> for () {}
+    impl Trait for u8 {}
+
+    // https://github.com/rust-lang/rust/blob/716394d6581b60c75cfdd88b8e5b876f2db88b62/src/test/ui/const-generics/defaults/const-param-as-default-value.rs
+    #[ext(Ext2)]
+    impl<const N: usize, const M: usize = N> () {}
+}
+
+#[test]
 fn macros() {
     macro_rules! m {
         (
