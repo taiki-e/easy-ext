aa974ca7faa3411692f9beb870f51b365f6aa31d
Taiki Endo <te316e89@gmail.com>
2023-01-08 00:22:02 +0900

    Change crate-level doc style

--- src/lib.rs
+++ src/lib.rs
@@ -1,173 +1,175 @@
-//! An attribute macro for easily writing [extension trait pattern][rfc0445].
-//!
-//! ```toml
-//! [dependencies]
-//! easy-ext = "1"
-//! ```
-//!
-//! *Compiler support: requires rustc 1.31+*
-//!
-//! # Examples
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext(ResultExt)]
-//! pub impl<T, E> Result<T, E> {
-//!     fn err_into<U>(self) -> Result<T, U>
-//!     where
-//!         E: Into<U>,
-//!     {
-//!         self.map_err(Into::into)
-//!     }
-//! }
-//! ```
-//!
-//! Code like this will be generated:
-//!
-//! ```rust
-//! pub trait ResultExt<T, E> {
-//!     fn err_into<U>(self) -> Result<T, U>
-//!     where
-//!         E: Into<U>;
-//! }
-//!
-//! impl<T, E> ResultExt<T, E> for Result<T, E> {
-//!     fn err_into<U>(self) -> Result<T, U>
-//!     where
-//!         E: Into<U>,
-//!     {
-//!         self.map_err(Into::into)
-//!     }
-//! }
-//! ```
-//!
-//! You can elide the trait name.
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext]
-//! impl<T, E> Result<T, E> {
-//!     fn err_into<U>(self) -> Result<T, U>
-//!     where
-//!         E: Into<U>,
-//!     {
-//!         self.map_err(Into::into)
-//!     }
-//! }
-//! ```
-//!
-//! Note that in this case, `#[ext]` assigns a random name, so you cannot
-//! import/export the generated trait.
-//!
-//! ## Visibility
-//!
-//! There are two ways to specify visibility.
-//!
-//! ### Impl-level visibility
-//!
-//! The first way is to specify visibility at the impl level. For example:
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! // unnamed
-//! #[ext]
-//! pub impl str {
-//!     fn foo(&self) {}
-//! }
-//!
-//! // named
-//! #[ext(StrExt)]
-//! pub impl str {
-//!     fn bar(&self) {}
-//! }
-//! ```
-//!
-//! ### Associated-item-level visibility
-//!
-//! Another way is to specify visibility at the associated item level.
-//!
-//! For example, if the method is `pub` then the trait will also be `pub`:
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext(ResultExt)] // generate `pub trait ResultExt`
-//! impl<T, E> Result<T, E> {
-//!     pub fn err_into<U>(self) -> Result<T, U>
-//!     where
-//!         E: Into<U>,
-//!     {
-//!         self.map_err(Into::into)
-//!     }
-//! }
-//! ```
-//!
-//! This is useful when migrate from an inherent impl to an extension trait.
-//!
-//! Note that the visibility of all the associated items in the `impl` must be identical.
-//!
-//! Note that you cannot specify impl-level visibility and associated-item-level visibility at the same time.
-//!
-//! ## [Supertraits](https://doc.rust-lang.org/reference/items/traits.html#supertraits)
-//!
-//! If you want the extension trait to be a subtrait of another trait,
-//! add `Self: SubTrait` bound to the `where` clause.
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext(Ext)]
-//! impl<T> T
-//! where
-//!     Self: Default,
-//! {
-//!     fn method(&self) {}
-//! }
-//! ```
-//!
-//! ## Supported items
-//!
-//! ### [Associated functions (methods)](https://doc.rust-lang.org/reference/items/associated-items.html#associated-functions-and-methods)
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext]
-//! impl<T> T {
-//!     fn method(&self) {}
-//! }
-//! ```
-//!
-//! ### [Associated constants](https://doc.rust-lang.org/reference/items/associated-items.html#associated-constants)
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext]
-//! impl<T> T {
-//!     const MSG: &'static str = "Hello!";
-//! }
-//! ```
-//!
-//! ### [Associated types](https://doc.rust-lang.org/reference/items/associated-items.html#associated-types)
-//!
-//! ```rust
-//! use easy_ext::ext;
-//!
-//! #[ext]
-//! impl str {
-//!     type Owned = String;
-//!
-//!     fn method(&self) -> Self::Owned {
-//!         self.to_owned()
-//!     }
-//! }
-//! ```
-//!
-//! [rfc0445]: https://github.com/rust-lang/rfcs/blob/HEAD/text/0445-extension-trait-conventions.md
+/*!
+An attribute macro for easily writing [extension trait pattern][rfc0445].
+
+```toml
+[dependencies]
+easy-ext = "1"
+```
+
+*Compiler support: requires rustc 1.31+*
+
+# Examples
+
+```rust
+use easy_ext::ext;
+
+#[ext(ResultExt)]
+pub impl<T, E> Result<T, E> {
+    fn err_into<U>(self) -> Result<T, U>
+    where
+        E: Into<U>,
+    {
+        self.map_err(Into::into)
+    }
+}
+```
+
+Code like this will be generated:
+
+```rust
+pub trait ResultExt<T, E> {
+    fn err_into<U>(self) -> Result<T, U>
+    where
+        E: Into<U>;
+}
+
+impl<T, E> ResultExt<T, E> for Result<T, E> {
+    fn err_into<U>(self) -> Result<T, U>
+    where
+        E: Into<U>,
+    {
+        self.map_err(Into::into)
+    }
+}
+```
+
+You can elide the trait name.
+
+```rust
+use easy_ext::ext;
+
+#[ext]
+impl<T, E> Result<T, E> {
+    fn err_into<U>(self) -> Result<T, U>
+    where
+        E: Into<U>,
+    {
+        self.map_err(Into::into)
+    }
+}
+```
+
+Note that in this case, `#[ext]` assigns a random name, so you cannot
+import/export the generated trait.
+
+## Visibility
+
+There are two ways to specify visibility.
+
+### Impl-level visibility
+
+The first way is to specify visibility at the impl level. For example:
+
+```rust
+use easy_ext::ext;
+
+// unnamed
+#[ext]
+pub impl str {
+    fn foo(&self) {}
+}
+
+// named
+#[ext(StrExt)]
+pub impl str {
+    fn bar(&self) {}
+}
+```
+
+### Associated-item-level visibility
+
+Another way is to specify visibility at the associated item level.
+
+For example, if the method is `pub` then the trait will also be `pub`:
+
+```rust
+use easy_ext::ext;
+
+#[ext(ResultExt)] // generate `pub trait ResultExt`
+impl<T, E> Result<T, E> {
+    pub fn err_into<U>(self) -> Result<T, U>
+    where
+        E: Into<U>,
+    {
+        self.map_err(Into::into)
+    }
+}
+```
+
+This is useful when migrate from an inherent impl to an extension trait.
+
+Note that the visibility of all the associated items in the `impl` must be identical.
+
+Note that you cannot specify impl-level visibility and associated-item-level visibility at the same time.
+
+## [Supertraits](https://doc.rust-lang.org/reference/items/traits.html#supertraits)
+
+If you want the extension trait to be a subtrait of another trait,
+add `Self: SubTrait` bound to the `where` clause.
+
+```rust
+use easy_ext::ext;
+
+#[ext(Ext)]
+impl<T> T
+where
+    Self: Default,
+{
+    fn method(&self) {}
+}
+```
+
+## Supported items
+
+### [Associated functions (methods)](https://doc.rust-lang.org/reference/items/associated-items.html#associated-functions-and-methods)
+
+```rust
+use easy_ext::ext;
+
+#[ext]
+impl<T> T {
+    fn method(&self) {}
+}
+```
+
+### [Associated constants](https://doc.rust-lang.org/reference/items/associated-items.html#associated-constants)
+
+```rust
+use easy_ext::ext;
+
+#[ext]
+impl<T> T {
+    const MSG: &'static str = "Hello!";
+}
+```
+
+### [Associated types](https://doc.rust-lang.org/reference/items/associated-items.html#associated-types)
+
+```rust
+use easy_ext::ext;
+
+#[ext]
+impl str {
+    type Owned = String;
+
+    fn method(&self) -> Self::Owned {
+        self.to_owned()
+    }
+}
+```
+
+[rfc0445]: https://github.com/rust-lang/rfcs/blob/HEAD/text/0445-extension-trait-conventions.md
+*/
 
 #![doc(test(
     no_crate_inject,
